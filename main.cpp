aaa09c8d0c5f24d8140110091b2a4a54700a41db
Santiago <smont015@ucr.edu>
2013-04-10 13:38:34 -0700

    Created 2D array.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,20 @@
 
 using namespace std;
 
+
 int main(){
+
+	int numbers[10][10];
+	int r = 0;
+	int c = 0;
+	for(int r = 0; r < 10; ++r)
+	{
+		for(c = 0; c < 10; ++c)
+		{
+			cout << numbers[r][c] << " ";
+		}
+		cout << endl;
+	}
 	cout << "Welcome to Fuzzy Adventure!" << endl;
 	return 0;
 }
